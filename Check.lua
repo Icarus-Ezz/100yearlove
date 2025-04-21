3ce57f606b6ddcf0ee5155549e0a18d8943f0d06
Icarus-Ezz <ngongochungphat@gmail.com>
2025-04-21 21:09:48 +0700

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -27,6 +27,7 @@ getgenv().config = {
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Icarus-Ezz/phatyeuem/refs/heads/main/Check.lua"))()
 ]]--
+wait(5)
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local HttpService = game:GetService("HttpService")
