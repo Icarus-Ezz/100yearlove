30cd47e5d9830d007f5495f5a39b77768185e040
Icarus-Ezz <ngongochungphat@gmail.com>
2025-04-21 20:54:13 +0700

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -21,8 +21,8 @@ getgenv().config = {
     Premium = {
 	["Auto Spawn Dark Beard"] = true,
         ["Auto Kill Dark Beard"] = true,    --Must Turn On Auto Spawn Dark Beard
-	["Auto Spawn Rip Indra"] = true,    --Need 3 Haki Lengend
-        ["Auto Kill Rip Indra"] = true,
+	["Auto Spawn Rip Indra"] = false,    --Need 3 Haki Lengend
+        ["Auto Kill Rip Indra"] = false,
     },
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Icarus-Ezz/phatyeuem/refs/heads/main/Check.lua"))()
