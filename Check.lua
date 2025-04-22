56c26fc0e922a65d53a48882d6283cec03000714
Icarus-Ezz <ngongochungphat@gmail.com>
2025-04-22 10:35:04 +0700

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -1075,7 +1075,7 @@ end
 ----------------------------------------------------------------------------------------------------
 local lastPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 local samePositionCount = 0
-local maxSamePositionCount = 20
+local maxSamePositionCount = 10
 
 function CheckForStuck()
     local currentPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
